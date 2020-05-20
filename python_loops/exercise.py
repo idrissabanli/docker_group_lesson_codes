f475f9703c6b4e133498f9851f061846309d4c37
idris <idris.sabanli@gmail.com>
2020-05-20 11:42:45 +0400

    strings

--- python_loops/exercise.py
+++
@@ -1,8 +0,0 @@
-# Bu gun ders 20:00-da basladi movzumuz Python Strings.
-# Python Strings dersi 20:00-da basladi.
-msg = "Bu gun ders 20:00-da basladi movzumuz Python Strings."
-
-print(f"{msg[38:44]} {msg[45:-1]} {msg[7:11]} {msg[27]} {msg[12:28]}.")
-
-
-# a=3 ile b = 2 reqemlerinin cemi a+b=3+2=5-dir
